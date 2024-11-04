22d45ccce9b62515b9c8b44d022407e0944f8058
VK <belsynth@gmail.com>
2024-11-04 20:07:28 +0300

    Добавляет реализацию camelize

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,6 @@
 function camelize(str) {
-  // ваш код...
+  return str
+    .split('-')
+    .map((value, index) => index === 0 ? value : value[0].toUpperCase() + value.slice(1))
+    .join('');
 }
