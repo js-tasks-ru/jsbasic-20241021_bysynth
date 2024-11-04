22bcb2eaae6288897a92ba674a577fecb20742b9
VK <belsynth@gmail.com>
2024-11-04 20:41:19 +0300

    Добавляет реализацию getMinMax

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbers = str
+    .split(' ')
+    .map(value => Number(value))
+    .filter(value => Number.isFinite(value));
+
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  };
 }
