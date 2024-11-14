bcc57774f41c10e86d1b4e347c73ad769ccbf373
VK <belsynth@gmail.com>
2024-11-14 18:24:40 +0300

    Добавляет реализацию makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+
+  friends.forEach(({firstName, lastName}) => {
+    ul.insertAdjacentHTML('beforeend', `<li>${firstName} ${lastName}</li>`);
+  });
+
+  return ul;
 }
