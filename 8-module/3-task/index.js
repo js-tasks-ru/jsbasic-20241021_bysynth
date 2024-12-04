56ca5c2015370d704cf91b491b7159159b6bcf8d
VK <belsynth@gmail.com>
2024-12-04 12:59:11 +0300

    Добавляет реализацию Cart часть 1

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,28 +6,49 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    if (product) {
+      let cartItem = this.cartItems.find((item) => item.product.id === product.id);
+
+      if (cartItem) {
+        this.updateProductCount(cartItem.product.id, 1);
+      } else {
+        cartItem = {
+          product: product,
+          count: 1
+        };
+
+        this.cartItems.push(cartItem);
+      }
+
+      this.onProductUpdate(cartItem);
+    }
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let cartItem = this.cartItems.find((item) => item.product.id === productId);
+    cartItem.count += amount;
+
+    if (cartItem.count === 0) {
+      this.cartItems.splice(this.cartItems.indexOf(cartItem), 1);
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    return this.cartItems.length === 0;
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((total, cartItem) => total + cartItem.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((total, cartItem) => total + cartItem.product.price * cartItem.count, 0);
   }
 
   onProductUpdate(cartItem) {
     // реализуем в следующей задаче
-
     this.cartIcon.update(this);
   }
 }
